e6b1f656084500398cbe575c066374fc83feebf7
ncradle <shnmy.mt555@gmail.com>
2022-05-12 23:26:27 +0900

    feat: 🎸 add c++ template file for atcoder

---
+++ template.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
+struct Fast {
+  Fast() {
+    std::cin.tie(0);
+    ios::sync_with_stdio(false);
+  }
+} fast;
+
+int main() { return 0; }
